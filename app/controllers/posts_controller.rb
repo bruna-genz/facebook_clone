34ad0921bf2fdc13d4d0b82131f23411de1a2aa8
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-01-14 13:03:08 -0500

    create post controller and respective logics

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+    before_action :
+
+    def new
+        @post = Post.new  
+    end
+
+    def create
+        @post = current_user.posts.build(posts_params)
+        if @post.valid?
+            @post.save
+            flash[:success] = "Post created"
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        
+    end
+
+    def update
+    end
+
+    def index
+    end
+
+    def destroy
+    end
+
+    private 
+
+        def posts_params
+            params.require(:post).permit(:content)
+        end
+
+        def logged_in_user
+            flash[:danger] = "You must sign in." unless user_signed_in?
+            redirect_to new_users_session_path unless user_signed_in?
+        end
+
+end
