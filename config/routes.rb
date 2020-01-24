d643474f61603ca3b1316377440edff9bd5948d4
Bruna <brunagenz92@gmail.com>
2020-01-24 17:58:09 -0400

    start styles of find friends view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  
- 
-  get 'comments/index'
-  get 'comments/create'
-  get 'comments/new'
-  get 'comments/destroy'
+
   root to: 'posts#index'
   get 'users/show', as: 'profile'
   get 'users/index', as: 'find_friends'
