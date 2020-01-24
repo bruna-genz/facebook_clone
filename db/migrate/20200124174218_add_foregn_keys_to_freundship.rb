3db0397acab7b3f28e1144cdbc8a0a51dfa606f2
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-01-24 15:45:22 -0500

    fix merge conflicts

--- db/migrate/20200124174218_add_foregn_keys_to_freundship.rb
+++
@@ -1,7 +0,0 @@
-class AddForegnKeysToFreundship < ActiveRecord::Migration[6.0]
-  def change
-    add_references :friendship, :user 
-    add_references :friendship, :friend
-    add_column :friendship, :confirmed, :boolean
-  end
-end
