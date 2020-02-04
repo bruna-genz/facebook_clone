25e9d84d4c99d3f8a2556125bf9469a59cda90a3
Bruna <brunagenz92@gmail.com>
2020-02-04 12:08:26 -0400

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, 
-         :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   # Validations
   validates :first_name, presence: true, length: { in: 2..30 }
@@ -56,24 +56,24 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      
+
       name = auth.info.name.split
       user.first_name = name[0]
       user.last_name = name[1] # assuming the user model has a name
       user.image_link = auth.info.image # assuming the user model has an image
       user.birthday = (Date.today - 18.years).to_s
-      user.gender = "Other"
-      # If you are using confirmable and the provider(s) you use validate emails, 
+      user.gender = 'Other'
+      # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
-      #user.skip_confirmation!
+      # user.skip_confirmation!
       user.save!
     end
   end
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if (data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"])
-        user.email = data["email"] if user.email.blank?
+      if (data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info'])
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
