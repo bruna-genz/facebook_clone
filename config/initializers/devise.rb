e01b8aaa7a90d9f6b89a41b57e49c94df4bc785e
Bruna <brunagenz92@gmail.com>
2020-02-05 17:15:39 -0400

    fix one more typo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "2877272292359665", "97de5720ad942138bf77d124cfe88ed6", callback_url: 'https://facebook-clone-bruna-nico.git/users/auth/facebook/callback'
+  config.omniauth :facebook, "2877272292359665", "97de5720ad942138bf77d124cfe88ed6", callback_url: 'https://facebook-clone-bruna-nico.com/users/auth/facebook/callback'
   
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
